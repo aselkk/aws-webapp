64aadf70b102fb6a9ecc20820a01d0d8a796dd77
asel.kurmanbekk <aselkurmanbekk@gmail.com>
2023-11-20 00:59:08 +0600

    add unit tests for handlers and openapi.yaml

---
+++ product-service/src/handlers/getProductsList.test.ts
@@ -0,0 +1,53 @@
+import { handler } from './getProductsList'
+import { buildResponse } from '../../helpers/utils'
+import products from '../mock-data/products.json'
+
+jest.mock('../../helpers/utils')
+
+describe('handler', () => {
+  it('should return 200 with product when product is found', async () => {
+    const productId = '1'
+    const event = {
+      pathParameters: {
+        productId,
+      },
+    }
+
+    const expectedProduct = products.find((p) => p.id === productId)
+    const expectedResponse = buildResponse(200, { product: expectedProduct })
+
+    const result = await handler(event)
+
+    expect(result).toEqual(expectedResponse)
+  })
+
+  it('should return 404 with "Product not found" message when product is not found', async () => {
+    const productId = 'nonexistent'
+    const event = {
+      pathParameters: {
+        productId,
+      },
+    }
+
+    const expectedResponse = buildResponse(404, { message: 'Product not found' })
+
+    const result = await handler(event)
+
+    expect(result).toEqual(expectedResponse)
+  })
+
+  it('should return 500 with error message when an error occurs', async () => {
+    const errorMessage = 'Internal server error'
+    const event = {}
+
+    jest.spyOn(products, 'find').mockImplementation(() => {
+      throw new Error(errorMessage)
+    })
+
+    const expectedResponse = buildResponse(500, { message: errorMessage })
+
+    const result = await handler(event)
+
+    expect(result).toEqual(expectedResponse)
+  })
+})
--- product-service/src/handlers/getProductsList.ts
+++ product-service/src/handlers/getProductsList.ts
@@ -4,7 +4,6 @@ import products from '../mock-data/products.json'
 export const handler = async (event:any) => {
 
   try {
-    console.log('hello', event)
     return buildResponse(200, {
       products
     })
